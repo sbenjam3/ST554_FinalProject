d355c1b62588764a4539d0191363bb5e25f12ea1
sbenjam3 <sbenjam3@ncsu.edu>
2026-04-27 11:38:16 -0400

    Add files via upload
    Add .py file to create streaming data.

---
+++ produce_stream_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from time import sleep 
+
+#load streaming data
+power_df = pd.read_csv('power_streaming_data.csv')
+
+for x in range(20):
+    #create new dataframe with 5 randomly selected records of data
+    sample_df = power_df.sample(n=5)
+    #drop sampled indicies from original dataframe to avoid resampling 
+    drop_index = sample_df.index.tolist()
+    power_df.drop(index=drop_index, inplace=True)
+    #save sample data to csv in the streaming_files folder
+    #set index to false to avoid writing out indicies to csv
+    sample_df.to_csv(f'streaming_files/batch_{x}', index=False)
+    #pause for 10 seconds in between writing new files
+    sleep(10)
+    
